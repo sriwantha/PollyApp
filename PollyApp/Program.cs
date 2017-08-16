11aad7a59dfcf1fdb20641f8c61b4fbe18d59397
sriwanth <sriwanth@SYD12-O9K98K3>
2017-08-16 16:57:14 +1000

    added code

--- PollyApp/Program.cs
+++ PollyApp/Program.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Amazon.Polly;
+using Amazon.Polly.Model;
+using NAudio.Wave;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +14,40 @@ namespace PollyApp
     {
         static void Main(string[] args)
         {
+            using (AmazonPollyClient client = new AmazonPollyClient())
+            {
+                while (true)
+                {
+                    string text = Console.ReadLine();
+                    if (!string.IsNullOrEmpty(text))
+                    {
+                        SynthesizeSpeechRequest request = new SynthesizeSpeechRequest();
+                        request.Text = text;
+                        request.TextType = TextType.Text;
+                        request.VoiceId = VoiceId.Amy;
+                        request.OutputFormat = OutputFormat.Mp3;
+                        SynthesizeSpeechResponse response = client.SynthesizeSpeech(request);
+                        using (MemoryStream memStream=new MemoryStream())
+                        {
+                            response.AudioStream.CopyTo(memStream);
+                            memStream.Position = 0;
+
+                            using (WaveStream blockAlignedStream =new BlockAlignReductionStream(WaveFormatConversionStream.CreatePcmStream(new Mp3FileReader(memStream))))
+                            {
+                                using (WaveOut waveOut = new WaveOut(WaveCallbackInfo.FunctionCallback()))
+                                {
+                                    waveOut.Init(blockAlignedStream);
+                                    waveOut.Play();
+                                    while (waveOut.PlaybackState == PlaybackState.Playing)
+                                    {
+                                        System.Threading.Thread.Sleep(100);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
